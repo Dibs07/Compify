c8715a0de8b914e781b6f14860f836ede2a6d3bc
sayan <sayanadh123@gmail.com>
2024-08-24 17:45:58 +0530

    modified:   client/components/common/Chat/ChatModal.tsx 	modified:   client/components/common/Chat/SubjectModal.tsx

--- client/components/common/Chat/ChatModal.tsx
+++ client/components/common/Chat/ChatModal.tsx
@@ -7,8 +7,8 @@ const ChatModal = ({ onClose }:{
 }) => {
     const [isModalOpen,setIsModalOpen] = useState(false);
     const handleCardClick:any = () => {
+        setIsModalOpen(true);
         onClose();
-            setIsModalOpen(true);
     };
 
     const closeModal = () => {
@@ -157,10 +157,10 @@ const ChatModal = ({ onClose }:{
             <button onClick={handleCardClick} className="text-white inline-flex w-full justify-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Next step
             </button>
-            {isModalOpen && <SubjectModal onClose={closeModal} />}
           </div>
         </div>
       </div>
+      {isModalOpen && <SubjectModal onClose={closeModal} />}
     </div>
   );
 };
--- client/components/common/Chat/SubjectModal.tsx
+++ client/components/common/Chat/SubjectModal.tsx
@@ -4,7 +4,7 @@ const SubjectModal = ({ onClose }: {
     onClose: any;
 }) => {
     return (
-        <div>
+     
 
             <div id="crypto-modal" tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div className="relative p-4 w-full max-w-md max-h-full">
@@ -63,7 +63,7 @@ const SubjectModal = ({ onClose }: {
                     </div>
                 </div>
             </div>
-        </div>
+        
     )
 }
 
