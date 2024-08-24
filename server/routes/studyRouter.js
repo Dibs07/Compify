fd78cf0b5ca9dff50c7d3b1b1406fb35bc351408
Dibs07 <banerjeedibs@gmail.com>
2024-08-24 17:06:55 +0530

    add:chat

--- server/routes/studyRouter.js
+++ server/routes/studyRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { generateStudyMaterials } from '../controllers/studyController.js';
+import { generateChatting } from '../controllers/chatControllers.js';
 
 const studyRouter = express.Router();
 
 studyRouter.post('/', generateStudyMaterials);
+studyRouter.post('/chat', generateChatting);
 
 
 export default studyRouter;
